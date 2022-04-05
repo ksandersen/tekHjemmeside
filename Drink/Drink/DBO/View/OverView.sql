57340bc7800e99f8b1e350c4da40ded9fb9e8785
LucasDahl10 <73586475+LucasDahl10@users.noreply.github.com>
2022-04-06 00:59:52 +0200

    DATABASE SAMT HTML OG CSS

---
+++ Drink/Drink/DBO/View/OverView.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW [dbo].[OverView]
+	AS 
+	
+	SELECT p.*
+	FROM dbo.Drinks p
+	
+	
+
---
+++ Drink/Drink/obj/Debug/postdeploy.sql
@@ -0,0 +1,16 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+BEGIN
+INSERT INTO dbo.Drinks (Gin, Vodka, Rom, Jager, Cola, Pris)
+VALUES (0,0,0,1,1,12)
+END
+GO
